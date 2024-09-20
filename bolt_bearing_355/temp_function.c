2cccc7bd8cdfb0e3134bdc99f49fe6d075c912a3
Alex Muraviev <muraviev-aa@yandex.ru>
2024-09-20 20:34:32 +0300

    Added support number of slices

--- bolt_bearing_355/temp_function.c
+++ bolt_bearing_355/temp_function.c
@@ -4,20 +4,29 @@
 void data_entry_dialog(WINDOW *sub1, WINDOW *a)
 {
     int bolt_diam;
+    int number_slices;
     int thick_first_part;
     int thick_second_part;
     char info_bolt_diam[3];
+    char info_number_slices[2];
     char info_thick_first_part[3];
     char info_thick_second_part[3];
     char ch;
-    wmove(sub1, 1, 1);
-    waddch(sub1, ACS_LANTERN);
+    wmove(sub1, 1, 7);
+    waddstr(sub1, "Initial data");
     wmove(sub1, 2, 1);
     waddch(sub1, ACS_LANTERN);
     wmove(sub1, 3, 1);
     waddch(sub1, ACS_LANTERN);
+    wmove(sub1, 4, 1);
+    waddch(sub1, ACS_LANTERN);
+    wmove(sub1, 5, 1);
+    waddch(sub1, ACS_LANTERN);
+    wmove(sub1, 6, 1);
+    waddch(sub1, ACS_LANTERN);
     wrefresh(sub1);
-    // Ввод диаметра болта
+
+    /* Ввод диаметра болта */
     do
     {
         wclear(a);
@@ -35,57 +44,90 @@ void data_entry_dialog(WINDOW *sub1, WINDOW *a)
             delete_char(a, 1, 1, 95);
     } while (ch != 'y');
     // Вывод результата ввода диаметра болта
-    wmove(sub1, 1, 3);
-    wprintw(sub1, "Data: bolt diameter is %d mm", bolt_diam);
-    wmove(sub1, 1, 1);
-    waddch(sub1, ACS_DIAMOND);
-    wrefresh(sub1);
-    // Ввод толщины первой сминаемой в одном направлении детали
-    do
-    {
-        wclear(a);
-        wbkgd(a, COLOR_PAIR(4));
-        wmove(a, 0, 2);
-        waddstr(a, "2.Enter the thickness of the first part (mm): ");
-        wgetnstr(a, info_thick_first_part, 2);
-        thick_first_part = atoi(info_thick_first_part);
-        wmove(a, 1, 4);
-        wprintw(a,
-                "Thickness of the first part is %d mm. If the information is correct then press 'y', if incorrect press 'n' ",
-                thick_first_part);
-        ch = (char) wgetch(a);
-        if (ch == 'n')
-            delete_char(a, 1, 1, 95);
-    } while (ch != 'y');
-    // Вывод результата ввода толщины первого элемента
-    wmove(sub1, 2, 9);
-    wprintw(sub1, "first part is %d mm", thick_first_part);
+    wmove(sub1, 2, 3);
+    wprintw(sub1, "bolt diameter is %d mm", bolt_diam);
     wmove(sub1, 2, 1);
     waddch(sub1, ACS_DIAMOND);
-    wrefresh(sub1);
-    // Ввод толщины второй сминаемой в одном направлении детали
-    do
-    {
-        wclear(a);
-        wbkgd(a, COLOR_PAIR(5));
-        wmove(a, 0, 2);
-        waddstr(a, "2.Enter the thickness of the second part (mm): ");
-        wgetnstr(a, info_thick_second_part, 2);
-        thick_second_part = atoi(info_thick_second_part);
-        wmove(a, 1, 4);
-        wprintw(a,
-                "Thickness of the second part is %d mm. If the information is correct then press 'y', if incorrect press 'n' ",
-                thick_second_part);
-        ch = (char) wgetch(a);
-        if (ch == 'n')
-            delete_char(a, 1, 1, 95);
-    } while (ch != 'y');
-    // Вывод результата ввода толщины второго элемента
-    wmove(sub1, 3, 9);
-    wprintw(sub1, "second part is %d mm", thick_second_part);
+    wmove(sub1, 3, 3);
+    wprintw(sub1, "strength class  is 8.8", bolt_diam);
     wmove(sub1, 3, 1);
     waddch(sub1, ACS_DIAMOND);
     wrefresh(sub1);
+
+    /* Ввод количества расчетных срезов одного болта */
+    do
+    {
+        wclear(a);
+        wbkgd(a, COLOR_PAIR(3));
+        wmove(a, 0, 2);
+        waddstr(a, "2.Enter number of slices: ");
+        wgetnstr(a, info_number_slices, 2);
+        number_slices = atoi(info_number_slices);
+        wmove(a, 1, 4);
+        wprintw(a,
+                "Number of slices is %d mm. If the information is correct then press 'y', if incorrect press 'n' ",
+                bolt_diam);
+        ch = (char) wgetch(a);
+        if (ch == 'n')
+            delete_char(a, 1, 1, 95);
+    } while (ch != 'y');
+    // Вывод количества расчетных срезов одного болта
+    wmove(sub1, 4, 3);
+    wprintw(sub1, "number of slices  is %d", number_slices);
+    wmove(sub1, 4, 1);
+    waddch(sub1, ACS_DIAMOND);
+    wrefresh(sub1);
+
+    if (number_slices == 1) // если один расчетный срез
+    {
+        /* Ввод толщины первой сминаемой в одном направлении детали */
+        do
+        {
+            wclear(a);
+            wbkgd(a, COLOR_PAIR(4));
+            wmove(a, 0, 2);
+            waddstr(a, "3.Enter the thickness of the first part (mm): ");
+            wgetnstr(a, info_thick_first_part, 2);
+            thick_first_part = atoi(info_thick_first_part);
+            wmove(a, 1, 4);
+            wprintw(a,
+                    "Thickness of the first part is %d mm. If the information is correct then press 'y', if incorrect press 'n' ",
+                    thick_first_part);
+            ch = (char) wgetch(a);
+            if (ch == 'n')
+                delete_char(a, 1, 1, 95);
+        } while (ch != 'y');
+        // Вывод результата ввода толщины первого элемента
+        wmove(sub1, 5, 3);
+        wprintw(sub1, "first part    is %d mm", thick_first_part);
+        wmove(sub1, 5, 1);
+        waddch(sub1, ACS_DIAMOND);
+        wrefresh(sub1);
+
+        /* Ввод толщины второй сминаемой в одном направлении детали */
+        do
+        {
+            wclear(a);
+            wbkgd(a, COLOR_PAIR(5));
+            wmove(a, 0, 2);
+            waddstr(a, "4.Enter the thickness of the second part (mm): ");
+            wgetnstr(a, info_thick_second_part, 2);
+            thick_second_part = atoi(info_thick_second_part);
+            wmove(a, 1, 4);
+            wprintw(a,
+                    "Thickness of the second part is %d mm. If the information is correct then press 'y', if incorrect press 'n' ",
+                    thick_second_part);
+            ch = (char) wgetch(a);
+            if (ch == 'n')
+                delete_char(a, 1, 1, 95);
+        } while (ch != 'y');
+        // Вывод результата ввода толщины второго элемента
+        wmove(sub1, 6, 3);
+        wprintw(sub1, "second part   is %d mm", thick_second_part);
+        wmove(sub1, 6, 1);
+        waddch(sub1, ACS_DIAMOND);
+        wrefresh(sub1);
+    }
 }
 
 void delete_char(WINDOW *w, int row, int column, int count_ch)
