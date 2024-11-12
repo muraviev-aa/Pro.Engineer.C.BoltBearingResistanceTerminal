a093bb72d1ad760f176158cf875057300fc92d31
Alex Muraviev <muraviev-aa@yandex.ru>
2024-11-12 07:13:30 +0300

    Added support C245

---
+++ bolt_bearing_245/main.c
@@ -0,0 +1,203 @@
+#include <curses.h>
+#include "temp_function.h"
+#include <stdio.h>
+#include <math.h>
+
+#define SIZE_STEEL 15           // число строк в файле tabl_B_3.csv
+#define SIZE_STEEL_ELEM 15      // число строк в файле tabl_G_6.csv
+#define SIZE_BOLT 5             // число строк в файле tabl_G_5.csv
+#define SIZE_BOLT_AREA 11       // число строк в файле tabl_G_9.csv
+#define USER 100
+#define USER1 101
+#define USER2 102
+#define USER3 103
+
+// Массив под входные данные
+int package_info[2];
+// Толщина сминаемой детали
+double thick_part;
+
+int main(void)
+{
+    WINDOW *sub1, *a, *b;
+    int maxx, maxy, halfx, halfy;
+    steel *info_st = (steel *) malloc(SIZE_STEEL * sizeof(steel));
+    steel_elem *info_st_el = (steel_elem *) malloc(SIZE_STEEL_ELEM * sizeof(steel_elem));
+    bolt *info_blt = (bolt *) malloc(SIZE_BOLT * sizeof(bolt));
+    bolt_area *info_blt_ar = (bolt_area *) malloc(SIZE_BOLT_AREA * sizeof(bolt_area));
+
+    if (!info_st || !info_blt || !info_st_el || !info_blt_ar)
+        printf("Error while allocating memory!\n");
+
+    FILE *fptr_st, *fptr_st_el, *fptr_blt, *fptr_blt_ar;
+
+    char file_name_st[] = "tabl_B_3.csv";
+    char file_name_st_el[] = "tabl_G_6.csv";
+    char file_name_blt[] = "tabl_G_5.csv";
+    char file_name_blt_ar[] = "tabl_G_9.csv";
+    int count_st;      // количество строк в файле tabl_B_3.csv
+    int count_st_el;   // количество строк в файле tabl_G_6.csv
+    int count_blt;     // количество строк в файле tabl_G_5.csv
+    int count_blt_ar;  // количество строк в файле tabl_G_9.csv
+    unsigned int r_u, r_bs, r_un, r_bp, r_bt, num_bolts;
+    double a_b, a_bn, max_sher_result, max_bear_result, max_tens_result,
+            total_shear_force, k_sher, k_tens, force_x, force_y, force_z;
+
+    initscr();
+
+    start_color();
+    if (!can_change_color())
+        addstr("This probably won't work...\n");
+    init_color(USER, 760, 960, 70);          // создаем салатовый цвет
+    init_color(USER1, 1000, 1000, 0);        // создаем ядовито желтый цвет
+    init_color(USER2, 0, 890, 420);          // создаем зеленый цвет
+    init_color(USER3, 840, 970, 690);        // создаем  салатово-желтый цвет
+
+    // Создание цветовых пар
+    init_pair(1, COLOR_BLUE, COLOR_WHITE);   // окно sub1 - базовый белый
+    init_pair(2, COLOR_WHITE, COLOR_BLUE);   // окно a - базовый синий
+    init_pair(3, COLOR_BLACK, USER);         // базовый салатовый
+    init_pair(4, COLOR_BLACK, USER1);        // базовый ядовито желтый цвет
+    init_pair(5, COLOR_BLACK, USER2);        // базовый зеленый цвет
+    init_pair(6, COLOR_BLACK, USER3);        // базовый  салатово-желтый цвет
+    init_pair(7, COLOR_WHITE, COLOR_BLUE);   // окно b - базовый синий
+    slk_color(0);
+
+    // Базовое окно терминала
+    bkgd(COLOR_PAIR(1));
+    refresh();
+
+    // Определяем размеры и положение доп. окон
+    getmaxyx(stdscr, maxy, maxx);
+    halfx = maxx >> 1;
+    halfy = maxy >> 1;
+
+    // Создаем доп. окна
+    sub1 = subwin(stdscr, maxy - 4, maxx - 2, 3, 1);     // окно введенных и полученных данных
+    a = subwin(stdscr, LINES - 28, COLS - 2, 1, 1);      // диалоговое окно
+    b = subwin(stdscr, halfy + 10, halfx - 1, 4, halfx); // окно для эскиза
+
+    if (sub1 == NULL || a == NULL || b == NULL)
+    {
+        endwin();
+        puts("Unable to create subwindow");
+        return (1);
+    }
+    wbkgd(a, COLOR_PAIR(2));
+    wbkgd(b, COLOR_PAIR(7));
+
+    // Работа с файлами
+    /* tabl_B_3.csv - данные по стали */
+    open_file(sub1, &fptr_st, file_name_st, 7);
+    count_st = read_data_file_steel(&fptr_st, info_st);
+    fclose(fptr_st);
+    /* tabl_G_5.csv - данные по болтам */
+    open_file(sub1, &fptr_blt, file_name_blt, 12);
+    count_blt = read_data_file_bolt(&fptr_blt, info_blt);
+    fclose(fptr_blt);
+    /* tabl_G_6.csv - расчетные сопротивления смятию элементов */
+    open_file(sub1, &fptr_st_el, file_name_st_el, 17);
+    count_st_el = read_data_file_steel_elem(&fptr_st_el, info_st_el);
+    fclose(fptr_st_el);
+    /* tabl_G_9.csv - площади сечения болтов */
+    open_file(sub1, &fptr_blt_ar, file_name_blt_ar, 22);
+    count_blt_ar = read_data_file_bolt_ar(&fptr_blt_ar, info_blt_ar);
+    fclose(fptr_blt_ar);
+
+    // 1. Вводим исходные данные
+    data_entry_dialog(sub1, a, b);
+
+    // 2. Читаем из полученных данных нужную информацию
+    r_u = design_steel_resistance_r_u(info_st, count_st);
+    r_un = design_steel_resistance_r_un(info_st, count_st);
+    r_bp = design_steel_resistance_r_bp(info_st_el, count_st_el, r_un);
+    r_bs = design_bolt_resistance_r_bs(info_blt, count_blt);
+    r_bt = design_bolt_resistance_r_bt(info_blt, count_blt);
+    a_b = bolt_a_b(info_blt_ar, count_blt_ar);
+    a_bn = bolt_a_bn(info_blt_ar, count_blt_ar);
+
+    // 3. Рисуем таблицу под данные из файлов для стали
+    draw_table(sub1, 9);
+
+    // 4. Рисуем таблицу под данные из файлов для болтов
+    draw_table(sub1, 16);
+
+    // 5. Заполняем данными таблицу
+    data_draw_table_steel(sub1, r_u, r_bp, r_un, 8);
+    data_draw_table_bolt(sub1, r_bs, r_bt, a_b, a_bn, 15);
+
+    // 6. Расчет на смятие
+    max_bear_result = calc_bearing_n_bp(r_bp);
+
+    // 7. Расчет на срез
+    max_sher_result = calc_bearing_n_bs(r_bs, a_b);
+
+    // 8. Расчет на растяжение
+    max_tens_result = calc_tens_n_bt(r_bt, a_bn);
+
+    // 9. Вывод результата расчета_1
+    output_results_1(sub1, max_sher_result, max_bear_result, max_tens_result);
+    wrefresh(sub1);
+
+    // 10. Рисуем систему координат
+    draw_coord_sys(b);
+
+    // 11. Вводим силу, действующую по оси Х
+    force_x = enter_force_x(sub1, a, 3);
+
+    // 12. Вводим силу, действующую по оси Y
+    force_y = enter_force_y(sub1, a, 4);
+
+    // 13. Вводим силу, действующую по оси Z
+    force_z = enter_force_z(sub1, a, b, 5);
+
+    // 14. Рисуем силы на системе координат
+    draw_force_coord_sys(b, force_x, force_y, force_z);
+
+    // 15. Ввод количества болтов в соединении
+    num_bolts = enter_num_bolts(b, a, 6);
+
+    // 16. Определение суммарного срезающего усилия
+    total_shear_force = sqrt(pow(force_x, 2) + pow(force_y, 2));
+    /* Вывод результата */
+    wmove(b, 20, 1);
+    wprintw(b, "Total shear force %.2f kN =", total_shear_force);
+    wmove(b, 21, 20);
+    wprintw(b, "= %.2f T", total_shear_force / 9.81);
+
+    // 17. Определение коэффициента использования по срезающему усилию
+    k_sher = sher_coefficient(b, num_bolts, max_sher_result, total_shear_force);
+
+    // 18. Определение коэффициента использования по смятию
+    bear_coefficient(b, num_bolts, max_bear_result, total_shear_force);
+
+    // 19. Определение коэффициента использования по растягивающему усилию
+    k_tens = tens_coefficient(b, force_z, num_bolts, max_tens_result);
+
+    // 20. Определение коэффициента использования по срезу и растяжению
+    sher_tens_coefficient(b, k_sher, k_tens);
+
+    wrefresh(sub1);
+    wrefresh(a);
+    wrefresh(b);
+    refresh();
+
+    getch();
+
+    // Освобождаем память
+    free(info_st);
+    free(info_st_el);
+    free(info_blt);
+    free(info_blt_ar);
+
+    endwin();
+    return 0;
+}
+
+
+
+
+
+
+
+
---
+++ bolt_bearing_245/temp_function.h
@@ -0,0 +1,175 @@
+#ifndef BOLT_BEARING_245_TEMP_FUNCTION_H
+#define BOLT_BEARING_245_TEMP_FUNCTION_H
+
+#include <curses.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define STEEL_NAME 245
+
+typedef struct
+{
+    unsigned int steel_name;       // марка стали
+    double thickness_1;            // толщина листа
+    double thickness_2;            // толщина листа
+    unsigned int r_yn;             // Ryn
+    unsigned int r_un;             // Run
+    unsigned int r_y;              // Ry
+    unsigned int r_u;              // Ru
+} steel;
+
+typedef struct
+{
+    unsigned int r_un;             // Run
+    unsigned int r_bp_a;           // Rbp класс точности А
+    unsigned int r_bp_b;           // Rbp класс точности B
+} steel_elem;
+
+typedef struct
+{
+    double class;            // класс прочности болта
+    unsigned int r_bun;      // Rbun
+    unsigned int r_byn;      // Rbyn
+    unsigned int r_bs;       // Rbs
+    unsigned int r_bt;       // Rbt
+} bolt;
+
+typedef struct
+{
+    unsigned int d;    // d
+    double a_b;        // Ab
+    double a_bn;       // Abn
+} bolt_area;
+
+/* Массив под входные данные
+ * package_info[0] - диаметр болта
+ * package_info[1] - количество срезов
+*/
+extern int package_info[2];
+
+// Наименьшая толщина (минимальная сумма толщин) детали, сминаемой в одну сторону
+extern double thick_part;
+
+// Открываем файл
+int open_file(WINDOW *name, FILE **fptr, char *name_file, int position);
+
+// Читаем файл c данными по стали
+int read_data_file_steel(FILE **fptr, steel info[]);
+
+// Читаем файл c данными по расч. сопротивлению сминаемых элементов
+int read_data_file_steel_elem(FILE **fptr_st_el, steel_elem info_st_el[]);
+
+// Читаем файл c данными по болтам
+int read_data_file_bolt(FILE **fptr, bolt info[]);
+
+// Читаем файл c данными по площадям болтов
+int read_data_file_bolt_ar(FILE **fptr, bolt_area info[]);
+
+// Добавляем запись площадей болтов
+void add_record_bolt_ar(bolt_area info[], int number, unsigned int d, double a_b, double a_bn);
+
+// Добавляем запись прочностных характеристик стали
+void add_record_steel(steel info[], int number, unsigned int steel_name, double thickness_1, double thickness_2,
+                      unsigned int r_yn, unsigned int r_un, unsigned int r_y, unsigned int r_u);
+
+// Добавляем запись прочностных характеристик сминаемых элементов
+void add_record_steel_elem(steel_elem info[], int number, unsigned int r_un, unsigned int r_bp_a,
+                           unsigned int r_bp_b);
+
+// Добавляем запись прочностных характеристик болтов
+void add_record_bolt(bolt info[], int number, double class, unsigned int r_bun, unsigned int r_byn,
+                     unsigned int r_bs, unsigned int r_bt);
+
+// Вводим исходные данные
+void data_entry_dialog(WINDOW *sub1, WINDOW *a, WINDOW *b);
+
+// Блок болта
+void block_bolt(WINDOW *b, int number_slices);
+
+// Блок шайбы
+void block_washer(WINDOW *b, int position);
+
+// Блок первой детали
+void first_part_block(WINDOW *b, int position);
+
+// Блок второй детали
+void second_part_block(WINDOW *b, int position);
+
+// Читаем из полученных данных Ru - расчетное сопротивление стали по временному сопротивлению
+unsigned int design_steel_resistance_r_u(const steel *info, int count);
+
+// Читаем из полученных данных Run - временное сопротивление стали
+unsigned int design_steel_resistance_r_un(const steel *info, int count);
+
+// Читаем из полученных данных Rbp - расчетное сопротивление смятию одноболтового соединения
+unsigned int design_steel_resistance_r_bp(const steel_elem *info_st_el, int count, unsigned int r_un);
+
+// Читаем из полученных данных Rbs - расчетное сопротивление срезу
+unsigned int design_bolt_resistance_r_bs(const bolt *info, int count);
+
+// Читаем из полученных данных Rbt - расчетное сопротивление растяжению
+unsigned int design_bolt_resistance_r_bt(const bolt *info, int count);
+
+// Читаем из полученных данных Ab - площадь сечения болта брутто
+double bolt_a_b(const bolt_area *info, int count);
+
+// Читаем из полученных данных Abn - площадь сечения болта нетто
+double bolt_a_bn(const bolt_area *info, int count);
+
+// Ввод толщин соединяемых деталей
+void enter_thick_info(WINDOW *a, int color_pair, char *arr, char ch,
+                      const char *text_1, const char *text_2);
+
+// Ввод силы по оси X
+double enter_force_x(WINDOW *sub1, WINDOW *a, int color_pair);
+
+// Ввод силы по оси Y
+double enter_force_y(WINDOW *sub1, WINDOW *a, int color_pair);
+
+// Ввод силы по оси Z
+double enter_force_z(WINDOW *sub1, WINDOW *a, WINDOW *b, int color_pair);
+
+// Ввод количества болтов в соединении
+unsigned int enter_num_bolts(WINDOW *b, WINDOW *a, int color_pair);
+
+// Рисуем таблицу
+void draw_table(WINDOW *sub1, int num);
+
+// Заполняем таблицу характеристиками стали
+void data_draw_table_steel(WINDOW *sub1, unsigned int r_u, unsigned int r_bp, unsigned int r_un, int num);
+
+// Заполняем таблицу характеристиками болта
+void data_draw_table_bolt(WINDOW *sub1, unsigned int r_bs, unsigned int r_bt, double a_b, double a_bn, int num);
+
+// Расчет на смятие / максимальное усилие на смятие
+double calc_bearing_n_bp(unsigned int r_bp);
+
+// Расчет на срез / максимальное усилие на срез
+double calc_bearing_n_bs(unsigned int r_bs, double a_b);
+
+// Расчет на растяжение / максимальное усилие на растяжение [кН]
+double calc_tens_n_bt(unsigned int r_bt, double a_bn);
+
+// Рисуем систему координат
+void draw_coord_sys(WINDOW *b);
+
+// Рисуем силы на системе координат
+void draw_force_coord_sys(WINDOW *b, double force_x, double force_y, double force_z);
+
+// Вывод результатов расчета_1
+void output_results_1(WINDOW *sub1, double max_sher_result, double max_bear_result, double max_tens_result);
+
+// Определение коэффициента использования по срезающему усилию
+double sher_coefficient(WINDOW *b, unsigned int num_bolts, double max_sher_result,
+                        double total_shear_force);
+
+// Определение коэффициента использования по смятию
+void bear_coefficient(WINDOW *b, unsigned int num_bolts, double max_bear_result, double total_shear_force);
+
+// Определение коэффициента использования по растяжению
+double tens_coefficient(WINDOW *b, double force_z, unsigned int num_bolts, double max_tens_result);
+
+// Определение коэффициента использования по срезу и растяжению
+void sher_tens_coefficient(WINDOW *b, double k_sher, double k_tens);
+
+#endif //BOLT_BEARING_245_TEMP_FUNCTION_H
